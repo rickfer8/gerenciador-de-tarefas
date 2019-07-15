357bf161789e66c6c36047e227917a361ed99eb9
Ricardo Ferreira Ribeiro <ricardo.ferib@gmail.com>
2019-07-14 21:43:58 -0300

    Criação das telas de listar cadastra editar e remover

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
-import { TarefaService } from './shared';
-import { ListarTarefaComponent } from './listar'
-
-
+import { TarefaService, TarefaConcluidaDirective } from './shared';
+import { ListarTarefaComponent } from './listar';
+import { CadastrarTarefaComponent } from './cadastrar';
+import { EditarTarefaComponent } from './editar';
 
 @NgModule({
-  declarations: [ListarTarefaComponent],
+  declarations: [ListarTarefaComponent, CadastrarTarefaComponent, EditarTarefaComponent, TarefaConcluidaDirective],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [
   	TarefaService
